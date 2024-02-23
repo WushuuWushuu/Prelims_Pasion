75e693dc270f698ac157187f2ade23a490adf01c
WushuuWushuu <135561492+WushuuWushuu@users.noreply.github.com>
2024-02-23 14:39:54 +0800

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+<header class="main_header">
+        <div class="header_top">
+            <h1 class="logo">Tonie Rose M Pasion</h1>
+            <a href="#" class="btn btn-rounded">Sign In</a>
+        </div>
+        <div class="header_content">
+            <h1>Peacful collection</h1>
+            <p><br>BSIT-3A</p>
+            <a href="#about" class="btn btn-xl">
+                Learn More
+                <i class="fas fa-chevron-right btn-icon"></i>
+            </a>
+        </div>
+           
+    </header>
+
+
+     
+
+    <section class="gallery">
+        <div class="gallery-container">    
+            
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/1.jpg">
+                    <div class="text-container">
+                      <p> Live</p>
+                    </div>
+                </div>
+                
+            </a>
+            
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/2.jpg">
+                    <div class="text-container">
+                        <p> The</p>
+                      </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/3.jpg" >
+                    <div class="text-container">
+                        <p> Life</p>
+                      </div>
+                </div>       
+            </a>
+
+ 
+          
+        </div>
+    </section>
+
+
+   
+    <section class="section-b" id="about">
+        <div class="overlay">
+            <div class="section-b-inner"> <br><br><br><br>
+                <h3> Yours Truly </h2>
+                <p>Thanks for visiting</p>
+            </div>
+        </div>
+    </section>
+
+
+   
+
+    
+
+
+    
+
+</body>
+</html>
+
+  
